2a3053d6c772eb78aa63f2a7b0f2cbbde05488c0
kennethlaqui <kennethlaqui@yahoo.com>
2021-02-21 11:43:44 +0800

    Add store for system token

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+// axios.defaults.baseURL = 'http://localhost:8000/api'
+
 export default new Vuex.Store({
   state: {
+    systemToken: localStorage.getItem('system_token') || null
+  },
+  getters: {
+    systemLoggedIn (state) {
+      return state.systemToken !== null
+    }
   },
   mutations: {
+    systemToken (state, payload) {
+      state.systemToken = payload
+    }
   },
   actions: {
+    // system token
+    async systemToken (context, credentials) {
+      try {
+        await new Promise((resolve, reject) => {
+          axios.post('s/login', {
+            email: credentials.email,
+            password: credentials.password
+          })
+            .then(response => {
+              console.log(response)
+              this.systemToken = response.data.access_token
+              localStorage.setItem('system_token', this.systemToken)
+              context.commit('systemToken', this.systemToken)
+              resolve(response)
+            })
+            .catch(error => {
+              localStorage.removeItem('system_token')
+              reject(error)
+            })
+        })
+      } catch (error) {
+        localStorage.removeItem('system_token')
+      }
+    }
   },
   modules: {
   }
