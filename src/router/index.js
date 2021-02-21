f2ce218903ae900a4f895000e1ab5c7b9b236910
kennethlaqui <kennethlaqui@yahoo.com>
2021-02-21 11:57:43 +0800

    Removed about route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../App.vue'
+import App from '../App.vue'
 import Signup from '../views/Authentication/Signup.vue'
 import Register from '../views/Authentication/Register.vue'
 import Signin from '../views/Authentication/Signin.vue'
@@ -10,16 +10,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'App',
+    component: App
   },
   {
     path: '/auth/signup',
@@ -35,7 +27,6 @@ const routes = [
     path: '/auth/signin',
     name: 'Signin',
     component: Signin
-
   }
 ]
 
