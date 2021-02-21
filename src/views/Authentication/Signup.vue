5a25c8fedcf438a0ccc4b41546da334e91352078
kennethlaqui <kennethlaqui@yahoo.com>
2021-02-21 11:52:46 +0800

    Remove term and condition

--- src/views/Authentication/Signup.vue
+++ src/views/Authentication/Signup.vue
@@ -4,44 +4,6 @@
 
     <Signup></Signup>
 
-    <v-row class="mt-1">
-
-      <v-col
-        align="center"
-        justify="center"
-      >
-
-        <!-- <h5 class="font-weight-regular grey--text">Forgot your password?</h5> -->
-        <v-row>
-
-          <v-col
-           align="center"
-           justify="center"
-          >
-
-            <h5>By clicking "Sign Up", you agree to Treasure's</h5>
-
-          </v-col>
-
-        </v-row>
-
-        <v-row class="mt-n5">
-
-           <v-col
-           align="center"
-           justify="center"
-          >
-
-            <h5>Terms and Conditions and Privacy Policy</h5>
-
-          </v-col>
-
-        </v-row>
-
-      </v-col>
-
-    </v-row>
-
   </div>
 
 </template>
