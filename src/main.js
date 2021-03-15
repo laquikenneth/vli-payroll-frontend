8710c18d2eca4482092340b8e31b56aa270baaa6
kennethlaqui <kennethlaqui@yahoo.com>
2021-03-16 02:28:35 +0800

    Add Axios interceptors

--- src/main.js
+++ src/main.js
@@ -14,6 +14,48 @@ Vue.config.productionTip = false
 
 axios.defaults.baseURL = 'https://vli-payroll-api-bziyh.ondigitalocean.app/api'
 
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('u_t')
+  const systemToken = localStorage.getItem('s_t')
+
+  // user token
+  if (token) {
+    config.headers = {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json'
+    }
+  }
+
+  // system token
+  if (systemToken) {
+    config.headers = {
+      Authorization: `Bearer ${systemToken}`,
+      Accept: 'application/json'
+    }
+  }
+  // Do something before request is sent
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  if (error.response.status !== 422) {
+    alert('Session Expired. Please sigin again.')
+  }
+  router.push({ name: 'Signin' })
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
+
 // route guard
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
@@ -22,7 +64,6 @@ router.beforeEach((to, from, next) => {
       if (!store.getters.loggedIn) {
         next({ name: 'Signin' })
       } else {
-        store.dispatch('authenticatedUser', 'User')
         next()
       }
     }
@@ -31,7 +72,6 @@ router.beforeEach((to, from, next) => {
       if (!store.getters.systemLoggedIn) {
         next({ name: 'System-Signin' })
       } else {
-        store.dispatch('authenticatedUser', 'System')
         next()
       }
     }
