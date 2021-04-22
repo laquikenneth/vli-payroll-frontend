5a1895b4c4b326ab6846e95dc3e6e5ef475142b7
kennethlaqui <kennethlaqui@yahoo.com>
2021-04-22 14:26:53 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
     'vuetify'
   ],
   devServer: {
+    public: 'https://vli-xk3s4.ondigitalocean.app'
     // proxy: {
     //   '^/api': {
     //     target: 'http://host.docker.internal:8000',
